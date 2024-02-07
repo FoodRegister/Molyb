45165a436a07f611fcd2571b4189cd5a6a2a8981
Mr Thimote <mrthimote@gmail.com>
2024-02-07 12:04:07 +0100

    2.x: experimented with tailwind

---
+++ examples/tailwind/index.tsx
@@ -0,0 +1,45 @@
+
+import { Component } from '../../molyb/component.js';
+import { Molyb, useDefaultConfig } from '../../molyb/molyb.js';
+import { DocumentType } from '../../vdom/virtualdom/virtual/document.js';
+import { Node } from '../../vdom/virtualdom/virtual/tree/node.js';
+
+class ButtonComponent extends Component {
+    name  : string;
+    count : number;
+
+    init()   : void {}
+    update() : Node<number, DocumentType<number>> | undefined {
+        let component = this;
+
+        return <div>
+            <button className="hover:text-blue-600 dark:text-gray-300" onclick={ () => {
+                component.count ++;
+
+                this.render(false);
+             } }>{ this.name }</button>
+            <span>You clicked { this.count } times !</span>
+        </div>
+    }
+
+    constructor (props: { name: string }) {
+        super();
+
+        this.name  = props.name;
+        this.count = 0;
+    }
+}
+
+class MainComponent extends Component {
+    init(): void {}
+    update(): Node<number, DocumentType<number>> | undefined {
+        return <div className="data-[type='false']:text-gray-100">
+            <ButtonComponent name="Button 1"></ButtonComponent>
+            <ButtonComponent name="Button 2"></ButtonComponent>
+        </div>
+    }
+}
+
+useDefaultConfig(document.body);
+
+Molyb.document.appendChild( <MainComponent></MainComponent> );
